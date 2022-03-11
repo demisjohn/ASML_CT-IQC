a118b1737ebefe6a58f7b7e7e8faebede4753790
demis <demis.john@gmail.com>
2022-03-10 23:48:35 -0800

    add Headless option (GUI-less MPL backend)

--- ASMLPlotTCData_v4.py
+++ ASMLPlotTCData_v4.py
@@ -5,7 +5,7 @@
 
     
 ASML Plot TC Data
-    Plot the historical temperature data from ASL's TC/CTlogM8477.cur
+    Plot the historical temperature data from ASML's TC/CTlogM8477.cur
     
 
     
@@ -20,10 +20,20 @@ import datetime
 #MinDate = "2021-06-30"      # Format: "2021-07-22" for July 22nd, 2021
 #MinTime = (0,0,0)      # Format: (16,30,0) for 16:30 (aka 4:30pm)
 #mindate = datetime.datetime.combine( datetime.date.fromisoformat( MinDate ) , datetime.time( *MinTime ) )
-mindate = datetime.datetime.now() - datetime.timedelta( days = 14 )  # plot data in the last 45 days, for example
+
+mindate = datetime.datetime.now() - datetime.timedelta( days = 7 )  # plot data in the last 45 days, for example
 mindate = datetime.datetime.combine( mindate.date() , datetime.time( 0,0,0 ) )  # set to midnight
-SaveFig = False;    # save the plot to a file?
+SaveFig = True;    # save the plot to a file?
 WS_ymin = None; #21.8      # set to None for auto
+PlotPressure = False
+PlotTCU = False
+ExportData  = False
+
+Headless = True  # running without a monitor
+if Headless:
+    import matplotlib
+    matplotlib.use('Agg')
+#end Headless
 
 '''
 # manually add TC .cur and .old files:
@@ -56,11 +66,14 @@ DataGrove_dir="/Users/demis/Documents/Characterizations, Tools, Equipment, Exper
 
 print('Running...')
 
-import ASML_CT  # Demis' custom ASML_CT analysis module, in ASML_CT.py
-import datetime # for date + time specification
-import glob     # for grabbing all filenames/paths in a directory.
+import ASML_CT
+import datetime
+import glob  #file path acquisition
+import os # file date acquisition
+import numpy
 
 
+## Find files to analyze ##
 folder = DataGrove_dir
 
 if not folder[-1] == "/":
@@ -68,6 +81,13 @@ if not folder[-1] == "/":
 
 TCFiles = glob.glob( folder + "*.cur.*" )    # find paths to each text file
 TCFiles.extend(  glob.glob( folder + "*.old.*" )  )
+
+# filter out files by creation date.
+TCctime = [  datetime.datetime.fromtimestamp(  os.path.getctime(f)  ) for f in TCFiles]
+TCctime_i = numpy.where(numpy.array(TCctime) >= mindate)[0]
+TCFiles = numpy.array(TCFiles)
+TCFiles = TCFiles[TCctime_i].tolist()
+
 
                
 IQCFiles = glob.glob( folder + "QICC.*" )
@@ -78,17 +98,26 @@ for i,f in enumerate(IQCFiles):
         IQCFileZ.append(f)
 #print("IQCFIleZ", IQCFileZ )
 
-
-ASML_CT.unset_DEBUG() # Turn off debugging output.  Use `set_DEBUG()` to enable.
-ct = ASML_CT.ASML_CT( TCFiles )   # analyze the files, return object `ct` containing the data.
-iqcdata = ct.add_IQC_files( IQCFileZ )  # add the ICQ files as well (different file format)
-ct.iqc_analyze()    # analyze the IQC data files - could probably auto-call this func by the above one.
-
-
+# filter out files by date.
+IQCctime = [  datetime.datetime.fromtimestamp(  os.path.getctime(f)  ) for f in IQCFileZ]  
+IQCctime_i = numpy.where(numpy.array(IQCctime) >= mindate)[0]
+IQCFileZ = numpy.array(IQCFileZ)
+IQCFileZ = IQCFileZ[IQCctime_i].tolist()
 
 
-fig = ct.plot(   WS_ymin=WS_ymin, data=ct.df[  ct.df['DateTime'] >  mindate  ]   , SaveFig=SaveFig )
+#raise UserError()
 
+
+ASML_CT.unset_DEBUG()
+ct = ASML_CT.ASML_CT( TCFiles )   # analyze the files
+iqcdata = ct.add_IQC_files( IQCFileZ )
+ct.iqc_analyze()
+
+
+
+
+fig = ct.plot(   data=ct.df[  ct.df['DateTime'] >  mindate  ]   , PlotPressure=PlotPressure, WS_ymin=WS_ymin,  SaveFig=SaveFig, PlotTCU=PlotTCU )
+if ExportData: ct.export_data()
 
 
 
